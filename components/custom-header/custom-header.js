0fc849965c7a5af5ca2b6b775bb82d078ad55b42
xuyong <xuyong@jachx.com>
2024-08-23 09:53:05 +0800

    feat:1.自定义头部导航栏高度调整

--- components/custom-header/custom-header.js
+++ components/custom-header/custom-header.js
@@ -1,52 +1,57 @@
-const navBarHeight = wx.getSystemInfoSync().statusBarHeight + 48;
+const navBarHeight = wx.getSystemInfoSync().platform == 'android' ? 48 : 44;
+const sysInfo = wx.getSystemInfoSync();
+const menuInfo = wx.getMenuButtonBoundingClientRect();
+const navigationBarHeight = (menuInfo.top - sysInfo.statusBarHeight) * 2 + menuInfo.height;
 Component({
-	properties: {
-		title: {
-			type: String,
-			value: '标题',
-		},
-		backgroundColor: {
-			type: String,
-			value: 'transparent',
-		},
-		titleColor: {
-			type: String,
-			value: '#212121',
-		},
-		leftIconType: {
-			type: String,
-			value: 'back',
-		},
-		Url: {
-			type: String,
-			value: ''
-		},
-		UrlNew: {
-			type: String,
-			value: ''
-		}
-	},
-	data: {
-		navBarHeight: navBarHeight,
-		statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
-  },
-	methods: {
-		goHome() {
-			this.triggerEvent('goHome');
-		},
-		goBack() {
-			if (this.properties.Url) {
-				wx.switchTab({
-					url: this.properties.Url,
-				})
+    properties: {
+        title: {
+            type: String,
+            value: '标题',
+        },
+        backgroundColor: {
+            type: String,
+            value: 'transparent',
+        },
+        titleColor: {
+            type: String,
+            value: '#212121',
+        },
+        leftIconType: {
+            type: String,
+            value: 'back',
+        },
+        Url: {
+            type: String,
+            value: ''
+        },
+        UrlNew: {
+            type: String,
+            value: ''
+        }
+    },
+    data: {
+        navigationBarHeight: navigationBarHeight,
+        navBarHeight: navBarHeight,
+        statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+    },
+    methods: {
+        goHome() {
+            this.triggerEvent('goHome');
+        },
+        goBack() {
+            if (this.properties.Url) {
+                wx.switchTab({
+                    url: this.properties.Url,
+                })
 
-			} else if (this.properties.UrlNew) {
-				wx.redirectTo({
-					url: this.properties.UrlNew,
-				})
-			} else {
-				wx.navigateBack();
-			}
-		}
-	}
+            } else if (this.properties.UrlNew) {
+                wx.redirectTo({
+                    url: this.properties.UrlNew,
+                })
+            } else {
+                wx.navigateBack();
+            }
+        },
+    },
+    onLoa
 });
